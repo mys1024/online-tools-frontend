ef9eae0fdfbb6e7704581a6b1c902023f1997e0c
mys1024 <mys.1024@outlook.com>
2021-11-04 16:50:14 +0800

    feat: update favicon

--- vite.config.ts
+++ vite.config.ts
@@ -98,25 +98,20 @@ export default defineConfig({
     // https://github.com/antfu/vite-plugin-pwa
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg', 'robots.txt', 'safari-pinned-tab.svg'],
+      includeAssets: ['favicon.ico', 'robots.txt'],
       manifest: {
         name: 'Vitesse',
         short_name: 'Vitesse',
         theme_color: '#ffffff',
         icons: [
           {
-            src: '/pwa-192x192.png',
-            sizes: '192x192',
+            src: '/pwa-200x200.png',
+            sizes: '200x200',
             type: 'image/png',
           },
           {
-            src: '/pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-          {
-            src: '/pwa-512x512.png',
-            sizes: '512x512',
+            src: '/pwa-200x200.png',
+            sizes: '200x200',
             type: 'image/png',
             purpose: 'any maskable',
           },
